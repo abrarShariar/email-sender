acadad9ab8d511e26e5f6029c44dc4db931d0386
abrarShariar <shariarabrar@gmail.com>
2018-10-17 16:31:32 +0600

    hacks

--- test.js
+++ test.js
@@ -2,10 +2,10 @@
 const mailer = require('./index');
 const userList = [
   { id: 1, firstname: 'Abrar', name: 'Abrar Shariar', email: 'shariarabrar@gmail.com' },
-  // { id: 2, firstname: 'Abrar', name: 'Abrar Shariar Galib', email: 'abrar@covalent.ai' },
-  // { id: 3, firstname: 'Tarik', name: 'Tarik Adnan Moon', email: 'tarik@covalent.ai' },
-  // { id: 4, firstname: 'Da Eun', name: 'Da Eun', email: 'daeun@covalent.ai' },
-  // { id: 5, firstname: 'Wenyi', name: 'Wenyi', email: 'wenyi@covalent.ai' }
+  { id: 2, firstname: 'Abrar', name: 'Abrar Shariar Galib', email: 'abrar@covalent.ai' },
+  { id: 3, firstname: 'Tarik', name: 'Tarik Adnan Moon', email: 'tarik@covalent.ai' },
+  { id: 4, firstname: 'Da Eun', name: 'Da Eun', email: 'daeun@covalent.ai' },
+  { id: 5, firstname: 'Wenyi', name: 'Wenyi', email: 'wenyi@covalent.ai' }
 ];
 
 mailer.sendMail(
